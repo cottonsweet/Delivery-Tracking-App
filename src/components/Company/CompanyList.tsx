cbbbeb0ea73d40fe5cd44968b7ec7816679fe73a
LK1 <64725837+sstipdev@users.noreply.github.com>
2023-04-06 07:17:10 +0000

    Create CompanyList Component

---
+++ src/components/Company/CompanyList.tsx
@@ -0,0 +1,21 @@
+
+// CSS
+import classes from "./CompanyList.module.css"
+
+
+
+const CompanyList = () => {
+
+
+    return (
+        <select>
+
+
+            <option>
+            </option>
+
+        </select>
+    )
+}
+
+export default CompanyList
