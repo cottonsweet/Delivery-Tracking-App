a07e5df37dfa914a52c988d74571ec75895ac044
LK1 <64725837+sstipdev@users.noreply.github.com>
2023-04-06 07:13:40 +0000

    Create Recycle Input Component

---
+++ src/components/Input/Input.tsx
@@ -0,0 +1,19 @@
+import React from "react"
+
+// CSS
+import classes from "./Input.module.css"
+
+interface Props {
+    type: string,
+    className: string,
+    placeholder: string,
+    onChange: (e: React.ChangeEvent<HTMLInputElement>) => void
+}
+
+const Input = (props: Props) => {
+    return (
+        <input type={props.type} className={`${classes[props.className]}`} placeholder={props.placeholder} onChange={props.onChange} />
+    )
+}
+
+export default Input
