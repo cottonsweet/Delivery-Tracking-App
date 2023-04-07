efef75d6e388436933ecc06822876812de7769f5
LK1 <64725837+sstipdev@users.noreply.github.com>
2023-04-07 07:25:19 +0000

    ColorList 컴포넌트 props 함수이벤트 처리 및 디자인 업데이트

--- src/components/Header/Color/ColorList.tsx
+++ src/components/Header/Color/ColorList.tsx
@@ -5,12 +5,16 @@ import Color from "./Color"
 // CSS
 import classes from "./ColorList.module.css"
 
+interface Props {
+    handleClickColorCode: (color: string) => void,
+}
 
-const ColorList = () => {
+const ColorList = (props: Props) => {
+
     return (
         <ul className={classes["color-item"]}>
             {Color.map((data) => (
-                <li key={data.id} className={classes["color-item__list"]}>{data.name}</li>
+                <li key={data.id} className={classes["color-item__list"]} onClick={() => props.handleClickColorCode(data.color)}>{data.name}</li>
             ))}
         </ul>
     )
