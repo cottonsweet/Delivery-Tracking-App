419461e5e0a9c4669ab5b458f8187d1ab66760cb
LK1 <64725837+sstipdev@users.noreply.github.com>
2023-04-07 05:00:34 +0000

    Color Item 데이터 배열 정의

---
+++ src/components/Header/Color/Color.ts
@@ -0,0 +1,35 @@
+interface ColorType {
+    id: number,
+    name: string,
+    color: string
+}
+
+const colorList: ColorType[] = [
+    {
+        id: 1,
+        name: "기본",
+        color: "#6366F1"
+    },
+    {
+        id: 2,
+        name: "다크",
+        color: "#111827"
+    },
+    {
+        id: 3,
+        name: "오렌지",
+        color: "#F97316"
+    },
+    {
+        id: 4,
+        name: "사이언",
+        color: "#06B6D4"
+    },
+    {
+        id: 5,
+        name: "핑크",
+        color: "#EC4899"
+    }
+]
+
+export default colorList
