d89fb4b3e84a581799d4d5960596ef85553e6f8a
LK1 <64725837+sstipdev@users.noreply.github.com>
2023-04-07 05:01:57 +0000

    운송장 번호 조회 결과 데이터 받아오기

--- src/components/Tracking/TrackingResult.tsx
+++ src/components/Tracking/TrackingResult.tsx
@@ -1,10 +1,34 @@
+import React from "react";
+
 // CSS
 import classes from "./TrackingResult.module.css"
 
-const TrackingResult = () => {
-    return(
-        <div></div>
+interface DetailType {
+    kind: string;
+    where: string;
+    timeString: string;
+}
+
+interface TrackingType {
+    trackingDetails: DetailType[];
+}
+
+interface Props {
+    trackingInfo?: TrackingType
+}
+
+const TrackingResult = (props: Props) => {
+    return (
+        <div>
+            {props.trackingInfo?.trackingDetails?.map((data, i) => (
+                <div key={i}>
+                    <div>{data.kind}</div>
+                    <div>{data.where}</div>
+                    <div>{data.timeString}</div>
+                </div>
+            ))}
+        </div>
     )
 }
 
-export default TrackingResult
+export default React.memo(TrackingResult)
