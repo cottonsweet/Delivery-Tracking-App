720d65221ac6a2dc9c436f8da5356dd158ada272
LK1 <64725837+sstipdev@users.noreply.github.com>
2023-04-07 05:01:35 +0000

    택배사 정보 데이터 불러오기

--- src/components/Company/CompanyList.tsx
+++ src/components/Company/CompanyList.tsx
@@ -1,19 +1,53 @@
+import { useState, useEffect } from "react"
 
 // CSS
 import classes from "./CompanyList.module.css"
 
+interface CompanyListType {
+    Code: string;
+    International: string;
+    Name: string;
+}
+
+interface CompanyType {
+    Company: CompanyListType[];
+}
+
+interface Props {
+    handleSetCompanyCode: (code: string) => void
+}
 
 
-const CompanyList = () => {
+const CompanyList = (props: Props) => {
+    const [companyList, setCompanyList] = useState<CompanyType>();
+
+    useEffect(() => {
+        getCompanyList();
+    }, []);
+
+
+    const BASE_URL = "https://info.sweettracker.co.kr/api/v1";
+    const API_KEY = "fwM3wePoMWuqF5k3n1f30Q";
+
+    /** 택배사 회사 리스트 불러오는 함수 */
+    const getCompanyList = async () => {
+        const data = await fetch(`${BASE_URL}/companylist?t_key=${API_KEY}`);
+        const json = await data.json();
+        setCompanyList(json);
+    };
+
+    /** 선택한 택배사 코드 저장하는 함수 */
+    const handleChangeCompanyCode = (e: React.ChangeEvent<HTMLSelectElement>) => props.handleSetCompanyCode(e.target.value);
 
 
     return (
-        <select>
-
-
-            <option>
-            </option>
-
+        <select onChange={handleChangeCompanyCode}>
+            <option>없음</option>
+            {companyList?.Company?.map((data, i) => (
+                <option key={i} value={data.Code}>
+                    {data.Name}
+                </option>
+            ))}
         </select>
     )
 }
