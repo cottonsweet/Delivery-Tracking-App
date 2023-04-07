9c82621d62242cad8ed454e82fa2e34ec270ec30
LK1 <64725837+sstipdev@users.noreply.github.com>
2023-04-07 07:03:15 +0000

    재사용 버튼 컴포넌트 props 데이터를 통한 동적 색상 출력

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -4,12 +4,15 @@ import classes from "./Button.module.css"
 interface Props {
     title: string,
     className?: string,
+    colorCode: string,
+    activiteBtnSystem: boolean,
     onClick?: () => void
 }
 
 const Button = (props: Props) => {
+    const buttonStyle = { backgroundColor: props.activiteBtnSystem ? props.colorCode : "#e5e7eb", color: props.activiteBtnSystem ? "" : "grey" }
     return (
-        <button onClick={props.onClick} className={classes[props.className || ""]}>{props.title}</button>
+        <button onClick={props.onClick} className={classes[props.className || ""]} style={buttonStyle} >{props.title}</button>
     )
 }
 
