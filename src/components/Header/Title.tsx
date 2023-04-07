621392bb00d70b65b340dbae0648d4cd9c53d6e4
LK1 <64725837+sstipdev@users.noreply.github.com>
2023-04-07 07:26:40 +0000

    재사용 Title 컴포넌트 props className 처리, 디자인 적용

--- src/components/Header/Title.tsx
+++ src/components/Header/Title.tsx
@@ -1,10 +1,14 @@
+// CSS
+import classes from "./Title.module.css"
+
 interface Props {
     title: string
+    className?: string
 }
 
 const Title = (props: Props) => {
     return (
-        <div>{props.title}</div>
+        <div className={classes[props.className || ""]}>{props.title}</div>
     )
 }
 
