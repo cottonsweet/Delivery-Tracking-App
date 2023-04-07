9cdd579364957ec9d7141766a777e8dc24a35742
LK1 <64725837+sstipdev@users.noreply.github.com>
2023-04-07 07:24:54 +0000

    재사용 Input 컴포넌트 props 데이터를 통한 동적 색상 출력

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -7,12 +7,13 @@ interface Props {
     type: string,
     className: string,
     placeholder: string,
+    colorCode: string,
     onChange: (e: React.ChangeEvent<HTMLInputElement>) => void
 }
 
 const Input = (props: Props) => {
     return (
-        <input type={props.type} className={`${classes[props.className]}`} placeholder={props.placeholder} onChange={props.onChange} />
+        <input type={props.type} className={`${classes[props.className]}`} placeholder={props.placeholder} onChange={props.onChange} style={{ border: "1px solid" + props.colorCode }} />
     )
 }
 
