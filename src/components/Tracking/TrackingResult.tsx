dcbe3e6abb4afdd10bf2315b9f24a06cbe9d2203
LK1 <64725837+sstipdev@users.noreply.github.com>
2023-04-08 10:28:21 +0000

    TrackingResult 컴포넌트 (배송결과 출력)

--- src/components/Tracking/TrackingResult.tsx
+++ src/components/Tracking/TrackingResult.tsx
@@ -3,6 +3,9 @@ import React from "react";
 // CSS
 import classes from "./TrackingResult.module.css"
 
+// Components
+import Button from "../Button/Button"
+
 interface DetailType {
     kind: string;
     where: string;
@@ -14,19 +17,37 @@ interface TrackingType {
 }
 
 interface Props {
-    trackingInfo?: TrackingType
+    trackingInfo?: TrackingType,
+    colorCode: string,
+    activiteBtnSystem: boolean,
+    handleTrackingModal: () => void
 }
 
 const TrackingResult = (props: Props) => {
+
+    const closeModal = () => {
+        props.handleTrackingModal()
+        console.log(1)
+    }
+
     return (
-        <div>
-            {props.trackingInfo?.trackingDetails?.map((data, i) => (
-                <div key={i}>
-                    <div>{data.kind}</div>
-                    <div>{data.where}</div>
-                    <div>{data.timeString}</div>
+        <div className={classes["tracking-result"]}>
+            <div className={classes['tracking-result__container']}>
+                <div className={classes["tracking-result__header"]} style={{ backgroundColor: props.colorCode }}>
+                    <div>현황</div>
+                    <div>위치</div>
+                    <div>시간</div>
                 </div>
-            ))}
+                {props.trackingInfo?.trackingDetails?.map((data, i) => (
+                    <div key={i} className={classes["tracking-result__data"]}>
+                        <div>{data.kind}</div>
+                        <div>{data.where}</div>
+                        <div>{data.timeString}</div>
+                    </div>
+                ))}
+
+            </div>
+            <Button title="닫기" className="close-btn" colorCode={props.colorCode} onClick={closeModal} activiteBtnSystem={props.activiteBtnSystem} />
         </div>
     )
 }
