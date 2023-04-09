4d69c20a2b2ef2cb9fab5e9a29bc5de49967ac64
LK1 <64725837+LAC-LK1@users.noreply.github.com>
2023-04-09 18:29:57 +0900

    :mute: remove console.log

--- src/components/Tracking/TrackingResult.tsx
+++ src/components/Tracking/TrackingResult.tsx
@@ -1,55 +1,50 @@
 import React from "react";
 
 // CSS
-import classes from "./TrackingResult.module.css"
+import classes from "./TrackingResult.module.css";
 
 // Components
-import Button from "../Button/Button"
+import Button from "../Button/Button";
 
 interface DetailType {
-    kind: string;
-    where: string;
-    timeString: string;
+  kind: string;
+  where: string;
+  timeString: string;
 }
 
 interface TrackingType {
-    trackingDetails: DetailType[];
+  trackingDetails: DetailType[];
 }
 
 interface Props {
-    trackingInfo?: TrackingType,
-    colorCode: string,
-    activiteBtnSystem: boolean,
-    handleTrackingModal: () => void
+  trackingInfo?: TrackingType;
+  colorCode: string;
+  activiteBtnSystem: boolean;
+  handleTrackingModal: () => void;
 }
 
 const TrackingResult = (props: Props) => {
+  const closeModal = () => props.handleTrackingModal();
 
-    const closeModal = () => {
-        props.handleTrackingModal()
-        console.log(1)
-    }
-
-    return (
-        <div className={classes["tracking-result"]}>
-            <div className={classes['tracking-result__container']}>
-                <div className={classes["tracking-result__header"]} style={{ backgroundColor: props.colorCode }}>
-                    <div>현황</div>
-                    <div>위치</div>
-                    <div>시간</div>
-                </div>
-                {props.trackingInfo?.trackingDetails?.map((data, i) => (
-                    <div key={i} className={classes["tracking-result__data"]}>
-                        <div>{data.kind}</div>
-                        <div>{data.where}</div>
-                        <div>{data.timeString}</div>
-                    </div>
-                ))}
-
-            </div>
-            <Button title="닫기" className="close-btn" colorCode={props.colorCode} onClick={closeModal} activiteBtnSystem={props.activiteBtnSystem} />
+  return (
+    <div className={classes["tracking-result"]}>
+      <div className={classes["tracking-result__container"]}>
+        <div className={classes["tracking-result__header"]} style={{ backgroundColor: props.colorCode }}>
+          <div>현황</div>
+          <div>위치</div>
+          <div>시간</div>
         </div>
-    )
-}
+        {props.trackingInfo?.trackingDetails?.map((data, i) => (
+          <div key={i} className={classes["tracking-result__data"]}>
+            <div>{data.kind}</div>
+            <div>{data.where}</div>
+            <div>{data.timeString}</div>
+          </div>
+        ))}
+      </div>
+      <Button title="닫기" className="close-btn" colorCode={props.colorCode} onClick={closeModal} activiteBtnSystem={props.activiteBtnSystem} />
+    </div>
+  );
+};
 
-export default React.memo(TrackingResult)
+export default React.memo(TrackingResult);
