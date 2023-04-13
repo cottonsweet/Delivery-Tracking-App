acf41dfc7a1070a5bd9d59d9d49678bffb1c5ac5
LK1 <64725837+LAC-LK1@users.noreply.github.com>
2023-04-13 23:56:44 +0900

    :bug: Fix a bug ColorList

--- src/components/Header/Color/ColorList.tsx
+++ src/components/Header/Color/ColorList.tsx
@@ -1,44 +1,50 @@
-import { useState } from "react"
+import { useState } from "react";
 
 // Type
-import { ColorType } from "./Color"
+import { ColorType } from "./Color";
 
 // Data
-import Color from "./Color"
+import Color from "./Color";
 
 // CSS
-import classes from "./ColorList.module.css"
+import classes from "./ColorList.module.css";
 
 interface Props {
-    handleClickColorCode: (color: string) => void,
-    colorCode: string,
+  handleClickColorCode: (color: string) => void;
+  colorCode: string;
 }
 
 const ColorList = (props: Props) => {
-    const [compareColor, setCompareColor] = useState("6366F1");
+  const [compareColor, setCompareColor] = useState("#6366F1");
 
-    const getActiviteColor = (color: string) => color === compareColor;
+  const getActiviteColor = (color: string) => color === compareColor;
 
-    // 헤더쪽 li 색상코드 데이터 받아오기
-    const handleChangeColor = (data: ColorType) => {
-        props.handleClickColorCode(data.color)
-        setCompareColor(data.color)
-    }
+  // 헤더쪽 li 색상코드 데이터 받아오기
+  const handleChangeColor = (data: ColorType) => {
+    props.handleClickColorCode(data.color);
+    setCompareColor(data.color);
+  };
 
-    // 받아온 색상 코드 데이터를 동적으로 className 지정
-    const getColorItemClass = (data: ColorType) => {
-        const isActive = getActiviteColor(data.color);
-        const defaultValue = `${data.color == compareColor ? classes["activiteColor"] : ""}`
-        return `${classes['color-item__list']} ${isActive ? classes['activiteColor'] : ''} ${defaultValue}`
+  // 받아온 색상 코드 데이터를 동적으로 className 지정
+  const getColorItemClass = (data: ColorType) => {
+    const isActive = getActiviteColor(data.color);
+    return `${classes["color-item__list"]} ${
+      isActive ? classes["activiteColor"] : ""
+    } `;
+  };
+  return (
+    <ul className={classes["color-item"]}>
+      {Color.map((data) => (
+        <li
+          key={data.id}
+          className={getColorItemClass(data)}
+          onClick={() => handleChangeColor(data)}
+        >
+          {data.name}
+        </li>
+      ))}
+    </ul>
+  );
+};
 
-    }
-    return (
-        <ul className={classes["color-item"]}>
-            {Color.map((data) => (
-                <li key={data.id} className={getColorItemClass(data)} onClick={() => handleChangeColor(data)}>{data.name}</li>
-            ))}
-        </ul>
-    )
-}
-
-export default ColorList
+export default ColorList;
