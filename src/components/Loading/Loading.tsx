fe26ca7d8ecceac476ee8ccf7e1d95e23f4980a8
LK1 <64725837+sstipdev@users.noreply.github.com>
2023-04-19 08:36:23 +0000

    로딩 컴포넌트 적용

---
+++ src/components/Loading/Loading.tsx
@@ -0,0 +1,21 @@
+import classes from "./Loading.module.css"
+
+const Loading = () => {
+    return (
+        <div className={classes["Loading-wrap"]}>
+            <div className={classes["container"]}>
+                <div className={classes["circle-container"]}>
+                    <div className={classes["circle"]}></div>
+                </div>
+                <div className={classes["circle-container"]}>
+                    <div className={classes["circle"]}></div>
+                </div>
+                <div className={classes["circle-container"]}>
+                    <div className={classes["circle"]}></div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Loading
