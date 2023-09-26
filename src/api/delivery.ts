b35284c9dc9d1a7ca095f7021608f7d3a86ed77c
LK1 <64725837+sstipdev@users.noreply.github.com>
2023-09-26 08:57:27 +0000

    Feat: 실시간 배송조회 API 로직 분리

--- src/api/delivery.ts
+++ src/api/delivery.ts
@@ -1,11 +1,35 @@
-
 const API_KEY = import.meta.env.VITE_REACT_APP_API_KEY;
 const BASE_URL = "https://info.sweettracker.co.kr/api/v1";
 
 // 택배사 모든 데이터 불러오기
 export const getAllCompanyList = async () => {
-    const deliveryCompany = `${BASE_URL}/companylist?t_key=${API_KEY}`
-    const data = await fetch(deliveryCompany);
-    const _json = await data.json();
-    return _json
+  const deliveryCompany = `${BASE_URL}/companylist?t_key=${API_KEY}`;
+  const data = await fetch(deliveryCompany);
+  const _json = await data.json();
+  return _json;
+};
+
+interface DetailType {
+  kind: string;
+  where: string;
+  timeString: string;
 }
+
+interface TrackingType {
+  trackingDetails: DetailType[];
+}
+
+// 배송조회 결과
+export const getDeliveryInqResult = async (
+  companyCode: string,
+  invoice: string
+) => {
+  if (companyCode === "") return alert("택배사를 선택 해주셔야 합니다.");
+
+  const data = await fetch(
+    `${BASE_URL}/trackingInfo?t_code=${companyCode}&t_invoice=${invoice}&t_key=${API_KEY}`
+  );
+  const _json = await data.json();
+  if (_json.status === false) return alert(_json.msg);
+  return _json;
+};
