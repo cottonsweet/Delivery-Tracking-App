5bdad9950fa4be1243945ef3fefa1802bfe9ac7f
LK1 <64725837+sstipdev@users.noreply.github.com>
2023-09-26 07:25:55 +0000

    Feat: 택배사 데이터 React-Query 마이그레이션

--- src/components/Company/CompanyList.tsx
+++ src/components/Company/CompanyList.tsx
@@ -1,17 +1,25 @@
-import React, { useState, useEffect } from "react";
+// React
+import React from "react";
 
 // CSS
 import classes from "./CompanyList.module.css";
+
+// api
+import { getAllCompanyList } from "../../api/delivery";
+
+// lib
+import { useQuery } from "react-query";
+
+// Components
 import Loading from "../Loading/Loading";
 
-interface CompanyListType {
-  Code: string;
-  International: string;
-  Name: string;
-}
 
 interface CompanyType {
-  Company: CompanyListType[];
+  Company: {
+    Code: string;
+    International: string;
+    Name: string;
+  }[]
 }
 
 interface Props {
@@ -20,35 +28,27 @@ interface Props {
 }
 
 const CompanyList = (props: Props) => {
-  const [companyList, setCompanyList] = useState<CompanyType>();
-  const [loading, setLoading] = useState(true)
-
-  useEffect(() => {
-    getCompanyList();
-  }, []);
-
-  const BASE_URL = "https://info.sweettracker.co.kr/api/v1";
-  const API_KEY = import.meta.env.VITE_REACT_APP_API_KEY;
 
   /** 택배사 회사 리스트 불러오는 함수 */
-  const getCompanyList = async () => {
-    setLoading(true)
-    const data = await fetch(`${BASE_URL}/companylist?t_key=${API_KEY}`);
-    const json = await data.json();
-    setCompanyList(json);
-    setLoading(false)
-  };
+  const getCompanyList = async ():Promise<CompanyType> =>  await getAllCompanyList();
+
+  const {
+    data: _comData,
+    isLoading: _comLoading,
+    isError: _comIsError,
+    error: _comError,
+  } = useQuery("company", getCompanyList);
 
   /** 선택한 택배사 코드 저장하는 함수 */
   const handleChangeCompanyCode = (e: React.ChangeEvent<HTMLSelectElement>) => props.handleSetCompanyCode(e.target.value);
 
   // 로딩컴포넌트 반환
-  if (loading) return <Loading />
+  if (_comLoading) return <Loading />
 
   return (
     <select onChange={handleChangeCompanyCode} className={classes["company-select"]} style={{ border: "1px solid" + props.colorCode }}>
       <option>없음</option>
-      {companyList?.Company?.map((data, i) => (
+      {_comData?.Company?.map((data, i) => (
         <option key={i} value={data.Code}>
           {data.Name}
         </option>
